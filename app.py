ecc388d4312f40507d11ed360b5fb0fb7907a6ba
mishagordeev <onitself@gmail.com>
2025-08-08 12:55:01 +0300

    Add content

--- app.py
+++ app.py
@@ -1,38 +1,21 @@
 
 from flask import Flask, render_template, redirect, url_for
 import markdown
+from pathlib import Path
 
 app = Flask(__name__, static_folder='styles')
 
+PAGES_DIR = Path("content")
+
 @app.route('/')
 def home():
-    return redirect(url_for('introduction'))
+    return redirect('/introduction')
 
-@app.route("/introduction")
-def introduction():
-    with open("content/introduction.md", "r", encoding="utf-8") as f:
-        md_text = f.read()
+@app.route("/<page_name>")
+def md_page(page_name):
+    filepath = PAGES_DIR / f"{page_name}.md"
     
-    html_content = markdown.markdown(md_text)
-
-    return render_template("index.html", content=html_content)
-
-@app.route("/character-creation")
-def character_creation():
-    with open("content/character-creation.md", "r", encoding="utf-8") as f:
-        md_text = f.read()
-    
-    html_content = markdown.markdown(md_text)
-
-    return render_template("index.html", content=html_content)
-
-@app.route("/rolling-the-dice")
-def rolling_the_dice():
-    with open("content/rolling-the-dice.md", "r", encoding="utf-8") as f:
-        md_text = f.read()
-    
-    html_content = markdown.markdown(md_text)
-
+    html_content = markdown.markdown(filepath.read_text(encoding="utf-8"))
     return render_template("index.html", content=html_content)
 
 if __name__ == '__main__':
