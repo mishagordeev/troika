c00cb9904f827df8bb2ad33d97e5120da0770ee9
mishagordeev <onitself@gmail.com>
2025-08-28 17:13:11 +0300

    Fix scroll blocking

--- static/js/mobile.js
+++ static/js/mobile.js
@@ -6,24 +6,43 @@ export function initMobileView() {
 
     if (!menuBtn || !sideMenu || !overlay) return;
 
+    let scrollPosition = 0;
+
     function toggleMenu(forceClose = false) {
+
         if (forceClose) {
             body.classList.remove('menu-open');
             overlay.style.display = 'none';
+
+            document.body.classList.remove('no-scroll');
+            document.body.style.top = '';
+            window.scrollTo(0, scrollPosition);
+
         } else {
             sideMenu.scrollTop = 0;
             body.classList.toggle('menu-open');
             overlay.style.display = body.classList.contains('menu-open') ? 'block' : 'none';
+
+            scrollPosition = window.scrollY;
+            document.body.classList.add('no-scroll');
+            document.body.style.top = `-${scrollPosition}px`;
         }
     }
 
-    menuBtn.addEventListener('click', () => toggleMenu());
+    menuBtn.addEventListener('click', function() {
+        if (body.classList.contains('menu-open')) {
+            toggleMenu(true)
+        } else {
+            toggleMenu();
+        }
+    });
 
     overlay.addEventListener('click', () => toggleMenu(true));
 
     document.querySelectorAll('.menu-item').forEach(item => {
         item.addEventListener('click', function() {
             if (window.innerWidth <= 768) {
+                scrollPosition = 0;
                 toggleMenu(true);
             }
         });
