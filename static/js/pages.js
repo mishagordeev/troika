6513e7dd86a1d3c5b8362bb9557beedc23b4da7b
mishagordeev <onitself@gmail.com>
2025-08-29 10:24:28 +0300

    Edit em style

--- static/js/pages.js
+++ static/js/pages.js
@@ -38,6 +38,13 @@ export function loadPage(path) {
         });
     }
 
+    document.querySelectorAll('em').forEach(em => {
+        em.innerHTML = em.innerHTML.replace(/([A-ZА-ЯЁ])/g, '<span class="capital-letter">$1</span>');
+        em.innerHTML = em.innerHTML.replace(/([1-9])/g, '<span class="numbers">$1</span>');
+    });
+    document.querySelectorAll('em').forEach(em => {
+        em.innerHTML = em.innerHTML.replace(/([1-9])/g, '<span class="numbers">$1</span>');
+    });
     generatePageNavigation(pages, page);
 }
 
--- static/js/style.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    document.querySelectorAll('em').forEach(em => {
-        em.innerHTML = em.innerHTML.replace(/([A-ZА-ЯЁ])/g, '<span class="capital-letter">$1</span>');
-        em.innerHTML = em.innerHTML.replace(/([1-9])/g, '<span class="numbers">$1</span>');
-    });
-    document.querySelectorAll('em').forEach(em => {
-        em.innerHTML = em.innerHTML.replace(/([1-9])/g, '<span class="numbers">$1</span>');
-    });
-});
